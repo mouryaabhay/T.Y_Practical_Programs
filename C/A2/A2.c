15dd5d879766a7fc87cd8bafce95cc6a30a83fb4
Mourya Abhay Amarjeet <mouryaabhay2426@gmail.com>
2026-02-02 08:22:46 +0530

    Refactor linked file allocation program: improve structure definitions, enhance memory management, and update user interface for file operations

--- C/A2/A2.c
+++ C/A2/A2.c
@@ -5,154 +5,172 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
-struct File_Details {
-    char File_Name[10];
-    int File_start;
-    int File_length;
-    int File_end;
-} F1[10];
+struct file_details {
+    char file_name[10];
+    int file_size;
+    int file_start;
+    int file_end;
+} directory[100];
 
-int Bit_vector[10], count = 0;
-int memory_size, free_blocks;
+int bit_vector[100];      // -1 = free block, otherwise stores next block index
+int memory_size, free_blocks, count = 0;
 
-void initial() {
+// Initialize memory
+void initialize_bit_vector() {
     int i;
     printf("Enter total number of memory blocks: ");
     scanf("%d", &memory_size);
+
+    for (i = 0; i < memory_size; i++)
+        bit_vector[i] = -1;   // all blocks free
+
     free_blocks = memory_size;
-    for (i = 0; i < memory_size; i++) {
-        Bit_vector[i] = -1; // -1 = free
-    }
 }
 
-void Display_bit_vector() {
+// Show memory status
+void show_memory_blocks() {
     int i;
-    printf("\nBit Vector (-1=free, other=link):\n");
-    for (i = 0; i < memory_size; i++) {
-        printf("%d ", Bit_vector[i]);
-    }
+    printf("\nMemory Blocks:\n");
+    for (i = 0; i < memory_size; i++)
+        printf("%d ", bit_vector[i]);
+
     printf("\nFree blocks: %d\n", free_blocks);
 }
 
-int Check_if_free_blocks_available(int needed) {
-    if (free_blocks >= needed) return 1;
-    return -1;
+// Update a block
+void update_bit_vector(int start_pos, int value) {
+    bit_vector[start_pos] = value;
 }
 
-void Update_directory(int start, int length, int end, char *name) {
-    strcpy(F1[count].File_Name, name);
-    F1[count].File_start = start;
-    F1[count].File_length = length;
-    F1[count].File_end = end;
+// Add file entry
+void add_directory_entry(char *filename, int filesize, int start_pos, int end_pos) {
+    strcpy(directory[count].file_name, filename);
+    directory[count].file_size = filesize;
+    directory[count].file_start = start_pos;
+    directory[count].file_end = end_pos;
     count++;
 }
 
-void Create_File() {
-    char name[10];
-    int length, i, blocks_allocated = 0, prev = -1, start = -1, rand_block;
+// Create file (Linked Allocation)
+void create_file() {
+    int i = 0, filesize, start_pos = -1, prev_pos = -1, curr_pos;
+    char filename[10];
 
-    printf("\nEnter File name: ");
-    scanf("%s", name);
-    printf("Enter File length (in blocks): ");
-    scanf("%d", &length);
+    printf("Enter file name: ");
+    scanf("%s", filename);
 
-    if (Check_if_free_blocks_available(length) == -1) {
-        printf("Error: Not enough free blocks.\n");
+    printf("Enter file size (blocks): ");
+    scanf("%d", &filesize);
+
+    if (filesize > free_blocks) {
+        printf("Enough memory not available\n");
         return;
     }
 
-    srand(time(0));
-    while (blocks_allocated < length) {
-        rand_block = rand() % memory_size;
-        if (Bit_vector[rand_block] == -1) {
-            if (blocks_allocated == 0) {
-                start = rand_block;
-            } else {
-                Bit_vector[prev] = rand_block;
+    while(i != filesize) {
+        curr_pos = rand() % memory_size;
+
+        if(bit_vector[curr_pos] == -1 && curr_pos != prev_pos) {
+            if(start_pos == -1)
+                start_pos = curr_pos; //first block of file
+            else
+                update_bit_vector(prev_pos, curr_pos);  //link previous block to current block
+
+            prev_pos = curr_pos;
+            i++; // one block is allocated in memory
+        }
+    }
+
+    update_bit_vector(prev_pos, -9); // end of file marker
+    add_directory_entry(filename, filesize, start_pos, prev_pos);
+
+    free_blocks -= filesize;
+
+    printf("File created successfully.\n");
+    printf("Start block: %d  End block: %d\n", start_pos, prev_pos);
+}
+
+// Delete file
+void delete_file() {
+    int i, j, start_pos, next_pos, temp;
+    char filename[10];
+
+    printf("Enter file name to delete: ");
+    scanf("%s", filename);
+
+    for (i = 0; i < count; i++) {
+        if (strcmp(directory[i].file_name, filename) == 0) {
+            start_pos = directory[i].file_start;
+            next_pos = start_pos;
+
+            while (next_pos != -9) {
+                temp = next_pos;
+                next_pos = bit_vector[temp];
+                bit_vector[temp] = -1;
             }
-            Bit_vector[rand_block] = -9;
-            prev = rand_block;
-            blocks_allocated++;
-            free_blocks--;
+
+            free_blocks -= directory[i].file_size;
+
+            for (j = i; j < count - 1; j++)
+                directory[j] = directory[j + 1];
+
+            count--;
+            printf("File deleted successfully\n");
+            return;
         }
     }
-    Bit_vector[prev] = -9;
-
-    Update_directory(start, length, prev, name);
-    printf("File '%s' created successfully.\n", name);
+    printf("File not found\n");
 }
 
-void Display_directory() {
+// Display directory
+void display_directory() {
     int i;
-    printf("\nDirectory:\n");
-    printf("Name\tStart\tEnd\tLength\n");
-    for (i = 0; i < count; i++) {
-        printf("%s\t%d\t%d\t%d\n", F1[i].File_Name, F1[i].File_start, F1[i].File_end, F1[i].File_length);
-    }
+    printf("\nFile Directory:\n");
+    printf("Name\tSize\tStart\tEnd\n");
+
+    for (i = 0; i < count; i++)
+        printf("%s\t%d\t%d\t%d\n",
+            directory[i].file_name,
+            directory[i].file_size,
+            directory[i].file_start,
+            directory[i].file_end);
 }
 
-int File_is_exist_or_not(char temp[]) {
-    int i;
-    for (i = 0; i < count; i++) {
-        if (strcmp(F1[i].File_Name, temp) == 0) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-void Delete_File() {
-    char name[10];
-    int i, index, current, next;
-
-    printf("\nEnter name to Delete the file: ");
-    scanf("%s", name);
-
-    index = File_is_exist_or_not(name);
-    if (index == -1) {
-        printf("Error: File not found.\n");
-        return;
-    }
-
-    current = F1[index].File_start;
-    while (current != -9) {
-        next = Bit_vector[current];
-        Bit_vector[current] = -1;
-        free_blocks++;
-        current = next;
-    }
-
-    for (i = index; i < count - 1; i++) {
-        F1[i] = F1[i + 1];
-    }
-    count--;
-
-    printf("File '%s' deleted successfully.\n", name);
-}
-
+// Main function
 int main() {
     int choice;
-    initial();
 
-    while (1) {
-        printf("\n1. Show Bit vector\n");
-        printf("2. Create New File\n");
-        printf("3. Show Directory\n");
-        printf("4. Delete File\n");
-        printf("5. Exit\n");
+    initialize_bit_vector();
+
+    do {
+        printf("\n1. Create File");
+        printf("\n2. Delete File");
+        printf("\n3. Display Directory");
+        printf("\n4. Show Memory Blocks");
+        printf("\n5. Exit");
         printf("\nEnter choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1: Display_bit_vector(); break;
-            case 2: Create_File(); break;
-            case 3: Display_directory(); break;
-            case 4: Delete_File(); break;
-            case 5: exit(0); break;
-            default: printf("\nInvalid Choice\n");
+        case 1:
+            create_file();
+            break;
+        case 2:
+            delete_file();
+            break;
+        case 3:
+            display_directory();
+            break;
+        case 4:
+            show_memory_blocks();
+            break;
+        case 5:
+            break;
+        default:
+            printf("Invalid choice\n");
         }
-    }
+    } while (choice != 5);
+
     return 0;
 }
