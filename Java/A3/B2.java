803bc1540db97ac9748ede4badffa0dfc1fe746b
Mourya Abhay Amarjeet <mouryaabhay2426@gmail.com>
2026-02-27 17:52:16 +0530

    Refactor database metadata display in B2.java for improved clarity and conciseness

--- Java/A3/B2.java
+++ Java/A3/B2.java
@@ -20,36 +20,14 @@ public class B2 {
             // Display Database Information
             System.out.println("\nDATABASE METADATA INFORMATION");
             System.out.println("========================================");
-
-            // 1. Database Product Name
             System.out.println("Database Product Name    : " + dbMetaData.getDatabaseProductName());
-
-            // 2. Database Product Version
             System.out.println("Database Product Version : " + dbMetaData.getDatabaseProductVersion());
-
-            // 3. Driver Name
             System.out.println("Driver Name              : " + dbMetaData.getDriverName());
-
-            // 4. Driver Version
             System.out.println("Driver Version           : " + dbMetaData.getDriverVersion());
-
-            // 5. User Name
             System.out.println("User Name                : " + dbMetaData.getUserName());
-
-            // Additional Information
-            System.out.println("\nADDITIONAL INFORMATION");
-            System.out.println("========================================");
-            System.out.println("JDBC Major Version       : " + dbMetaData.getJDBCMajorVersion());
-            System.out.println("JDBC Minor Version       : " + dbMetaData.getJDBCMinorVersion());
-            System.out.println("URL                      : " + dbMetaData.getURL());
-            System.out.println("Max Connections          : " + dbMetaData.getMaxConnections());
-            System.out.println("Supports Transactions    : " + dbMetaData.supportsTransactions());
-            System.out.println("Supports Stored Procedures: " + dbMetaData.supportsStoredProcedures());
-            System.out.println("Supports Batch Updates   : " + dbMetaData.supportsBatchUpdates());
 
             con.close();
             System.out.println("\nConnection closed");
-
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             e.printStackTrace();
